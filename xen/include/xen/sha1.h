f468b80551ac7432e6e722e599f9419bcabf7fc7
Krystian Hebel <krystian.hebel@3mdeb.com>
2022-10-25 16:04:17 +0200

    lib/sha1.c: add file
    The code comes from [1] and is licensed under GPL-2.0 license.
    It's a combination of:
     - include/crypto/sha1.h
     - include/crypto/sha1_base.h
     - lib/crypto/sha1.c
     - crypto/sha1_generic.c
    
    Changes:
     - includes
     - formatting
     - renames and splicing of some trivial functions that are called once
     - dropping of `int` return values (only zero was ever returned)
     - getting rid of references to `struct shash_desc`
    
    [1]: https://github.com/torvalds/linux/tree/afdab700f65e14070d8ab92175544b1c62b8bf03
    
    Signed-off-by: Sergii Dmytruk <sergii.dmytruk@3mdeb.com>
    Signed-off-by: Krystian Hebel <krystian.hebel@3mdeb.com>

---
+++ xen/include/xen/sha1.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+#ifndef __XEN_SHA1_H
+#define __XEN_SHA1_H
+
+#include <xen/inttypes.h>
+
+#define SHA1_DIGEST_SIZE  20
+
+void sha1_hash(const u8 *data, unsigned int len, u8 *out);
+
+#endif /* !__XEN_SHA1_H */
